70e0abf56d55da9958094ee2c4fc9c3f80c15616
Edison Giraldo A <1470@holbertonschool.com>
2020-05-14 04:22:00 +0000

    identify command and returns function

--- instruction_func.c
+++ instruction_func.c
@@ -1,3 +1,4 @@
+#include "global.h"
 #include "monty.h"
 
 /**
@@ -5,53 +6,53 @@
  * file, finds the command and returns the pointer to
  * function that executes that command.
  *
- * @buf: contents of monty file.
+ * @str: contents of monty file.
  * @line_number: number of line to analyse.
  *
  * Return: pointer to function of instruction or NULL if fails.
  */
-void (*instruction_func(char *buf, int line_number))(stack_t **, unsigned int)
+void (*instruction_func(char *str, int line_number))(stack_t **, unsigned int)
 {
-        instruction_t name_func[] = {{"push", push}, {"pall", pall}, {NULL, NULL}};
-        char *instruction = NULL;
-        int i = 0, match = 0;
+	instruction_t name_func[] = {{"push", push}, {"pall", pall}, {NULL, NULL}};
+	char *instruction = NULL;
+	int i = 0, match = 0;
 
-        if (*buf == '\0')
-                return (NULL);
+	if (*str == '\0')
+		return (NULL);
 
-        while (line_number != 0)/*look for line*/
-        {
-                if (*buf == '\n')
-                        line_number--;
-                else if (*buf == '\0')
-                        return (NULL);
-                buf++;
-        }
+	while (line_number != 0)/*look for line*/
+	{
+		if (*str == '\n')
+			line_number--;
+		else if (*str == '\0')
+			return (NULL);
+		str++;
+	}
 
-        while (*buf < 97 && *buf > 122)/*look for first instruction letter*/
-        {/*     what if there is an strange symbol before the instruction?*/
-                if (*buf == '\n')/*Error if any instruction is not find*/
-                        return (NULL);
-                buf++;
-        }
+	while (*str < 97 && *str > 122)/*look for first instruction letter*/
+	{/*     what if there is an strange symbol before the instruction?*/
+		if (*str == '\n')/*Error if any instruction is not find*/
+			return (NULL);
+		str++;
+	}
 
-        instruction = malloc(10 * sizeof(char));
+	instruction = malloc(10 * sizeof(char));
 
-        while (buf[i] != '\n' && buf[i] != ' ' && i < 9)/*get instruction*/
-        {
-                instruction[i] = buf[i];
-                i++;
-        }
-        instruction[i] = '\0';
+	while (str[i] != '\n' && str[i] != ' ' && i < 9)/*get instruction*/
+	{
+		instruction[i] = str[i];
+		i++;
+	}
+	instruction[i] = '\0';
 
-        i = 0;
-        while ((name_func[i]).opcode != NULL && match == 0)
-        {
-                match = _strcmp(instruction, (name_func[i]).opcode);
-                if (match == 0)
-                        i++;
-        }
+	i = 0;
+	while ((name_func[i]).opcode != NULL && match == 0)
+	{
+		match = _strcmp(instruction, (name_func[i]).opcode);
+		if (match == 0)
+			i++;
+	}
 
-        free(instruction);
-        return ((name_func[i]).f);
+	free(instruction);
+	return ((name_func[i]).f);
 }
