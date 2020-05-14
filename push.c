46727febe8922f2b884959cde92ce348d0bfbf71
Edison Giraldo A <1470@holbertonschool.com>
2020-05-14 04:23:03 +0000

    add elements to stack

--- push.c
+++ push.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+#include "global.h"
 
 /**
  * push - pushes an element to the stack.
@@ -9,20 +10,31 @@
 
 void push(stack_t **stack, unsigned int line_number)
 {
-/*        int bytes_in_line = 0;*/
-/**
-*        while (line_number != 0)
-*        {
-*                if (*buf == '\n')
-*                        line_number--;
-*                else if (*buf == '\0')
-*                        return (NULL);
-*                buf++;
-*        }
-*/
+	int bytes_in_line = 0, n = 0;
+	extern char *buf;
+	char *line = buf;
 
-        printf("push\n");
-        if (stack == NULL)
-                printf("stack is NULL\n");
-        printf("ln: %u\n", line_number);
+	while (line_number != 0)
+	{
+		if (*line == '\n')
+			line_number--;
+		else if (*line == '\0')
+			return;
+		line++;
+	}
+
+	while (*line < 48 || *line > 57)
+		line++;
+
+	while (line[bytes_in_line] != '\n')
+		bytes_in_line++;
+
+	line[bytes_in_line] = '\0';
+
+	n = atoi(line);
+	/* printf("line: %s, n: %d\n", line, n); */
+
+	add_dnodeint(stack, n);
+
+	line[bytes_in_line] = '\n';
 }
