57fca3b0169ebb76aa9d113ccb229aab1e4ec0c2
Edison Giraldo A <1470@holbertonschool.com>
2020-05-14 02:01:22 +0000

    Function that prints errors and finish the program

---
+++ error_exit.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+/**
+ * error_exit - prints error message and exit
+ * with EXIT_FAILURE status.
+ *
+ * @message1: error message to print part 1.
+ * @message2: error message to print part 2.
+ */
+
+void error_exit(char *message1, char *message2)
+{
+        if (message2 == NULL)
+                message2 = "";
+
+        fprintf(stderr, "%s", message1);
+        fprintf(stderr, "%s\n", message2);
+        exit(EXIT_FAILURE);
+}
