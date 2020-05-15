2ab3ed48b5b9b06a287929ab2e022d725c825901
Edison Giraldo A <1470@holbertonschool.com>
2020-05-15 03:10:30 +0000

    The opcode nop doesn’t do anything.

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * nop - doesn't do nothing.
+ *
+ * @stack: double pointer to head of stack.
+ * @line_number: number of line where the instruction is.
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
