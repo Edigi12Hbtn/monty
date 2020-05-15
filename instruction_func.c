c8a10e6649c0c0f06b79965de9e92299b153cb92
Edison Giraldo A <1470@holbertonschool.com>
2020-05-15 03:09:33 +0000

    get instruction and returns the corresponding function

--- instruction_func.c
+++ instruction_func.c
@@ -13,7 +13,8 @@
 void (*instruction_func(char *instruction))(stack_t **, unsigned int)
 {
 	instruction_t name_func[] = {{"push", push}, {"pall", pall},
-	{"pint", pint}, {"pop", pop}, {NULL, NULL}};
+	{"pint", pint}, {"pop", pop}, {"swap", swap}, {"add", add},
+	{"nop", nop}, {NULL, NULL}};
 	int i = 0, match = 0;
 
 	while ((name_func[i]).opcode != NULL && match == 0)
