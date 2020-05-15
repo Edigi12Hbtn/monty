1713845c838b5ee3e0fda227a2f0a7e1012bfc39
Edison Giraldo A <1470@holbertonschool.com>
2020-05-15 03:42:07 +0000

    The opcode mod computes the rest of the division of the second top element of the stack by the top element of the stack.

--- mod.c
+++ mod.c
@@ -15,7 +15,11 @@ void mod(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%u: can't mod, stack too short\n", line_number + 1);
 		exit(EXIT_FAILURE);
 	}
-
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number + 1);
+		exit(EXIT_FAILURE);
+	}
 	((*stack)->next)->n %= (*stack)->n;
 	pop(stack, line_number);
 }
